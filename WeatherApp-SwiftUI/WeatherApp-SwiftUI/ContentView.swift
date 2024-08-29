3410fd8cab6af4e4db793dc1af79f9d564bc543a
Muhammed Rashid <rashidbinlatheef@gmail.com>
2024-08-29 19:33:00 +0530

    [CHORE] Created Initial set up

--- WeatherApp-SwiftUI/WeatherApp-SwiftUI/ContentView.swift
+++ WeatherApp-SwiftUI/WeatherApp-SwiftUI/ContentView.swift
@@ -9,13 +9,86 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue, .white],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea(edges: .all)
+            VStack(spacing: 40) {
+                Text("Cupertino, CA")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                Spacer()
+                VStack {
+                    Image(systemName: "sunset")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 140)
+                    Text("76o")
+                        .font(.title)
+                        .bold()
+                        .foregroundStyle(.white)
+                }
+                HStack (spacing: 10){
+                    VStack {
+                        Text("TUE")
+                        Image(systemName: "sunrise")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        Text("74o")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                    }
+                    VStack {
+                        Text("TUE")
+                        Image(systemName: "sunrise")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        Text("74o")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                    }
+                    VStack {
+                        Text("TUE")
+                        Image(systemName: "sunrise")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        Text("74o")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                    }
+                    VStack {
+                        Text("TUE")
+                        Image(systemName: "sunrise")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        Text("74o")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                    }
+                    VStack {
+                        Text("TUE")
+                        Image(systemName: "sunrise")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        Text("74o")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                    }
+                }
+                Spacer()
+
+                Button("Change Time of Day") {
+                    print("Buttn Tapped")
+                }.foregroundColor(.blue)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
