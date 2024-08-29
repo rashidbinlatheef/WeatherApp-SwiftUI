450221f5cd789658c68c75417936c60053617e04
Muhammed Rashid <rashidbinlatheef@gmail.com>
2024-08-29 19:14:56 +0530

    [CHORE] Created Project

---
+++ WeatherApp-SwiftUI/WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Muhammed Rashid on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
